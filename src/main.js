8dd6bacc3ea035ca39d44e6cc9b4e697d9294d58
Raiu1210 <baio1484@gmail.com>
2019-10-07 16:06:42 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC41A9F0Cd6qLDnS1CWWTJFtXMlOQU9FU8",
+  authDomain: "vue-firebase-chat-9b72e.firebaseapp.com",
+  databaseURL: "https://vue-firebase-chat-9b72e.firebaseio.com",
+  projectId: "vue-firebase-chat-9b72e",
+  storageBucket: "vue-firebase-chat-9b72e.appspot.com",
+  messagingSenderId: "108722078339",
+  appId: "1:108722078339:web:abc909eb25dc3c514ed1b6",
+  measurementId: "G-H33RVXTG8N"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
